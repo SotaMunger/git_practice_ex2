b152f0667a8ba079e0c57ada3895a7d640f32d4d
Richard Bridges <91567553+SotaMunger@users.noreply.github.com>
2023-10-02 12:36:29 -0400

    Update server.R

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ server <- function(input, output) {
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    hist(x, breaks = bins, col = input$colors, border = "white",
+    hist(x, breaks = bins, col = "#007bc2", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
     
